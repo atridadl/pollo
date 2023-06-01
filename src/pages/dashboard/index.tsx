6b74c7d6f0c78dfccec1ffe2a9588ff82dc21809
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-05-31 21:53:19 -0600

    Remove unused vars and cut a release

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
-import { GetServerSideProps, type NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 import RoomList from "~/components/RoomList";
 
