a2ac8da8177bac79aa7f902b0d7bcfd63ace8a2b
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-03 01:35:40 -0600

    Turns out you can just use Preact...

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,17 @@ const config = {
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 const nextConfig = withPWA({
   dest: "public",
