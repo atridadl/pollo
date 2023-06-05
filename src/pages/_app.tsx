766a8ff1dbba53536ef46d88a838452d469ecb5d
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-05 12:37:10 -0600

    Quick style updates and dashboard tab selection memory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <div className="block h-[100%]">
         <Navbar title="Sprint Padawan" />
         <div className="flex flex-row items-center justify-center min-h-[calc(100%-114px)]">
-          {pageLoading ? <Loading /> : <Component {...pageProps} />}
+          {pageLoading ? (
+            <span className="loading loading-spinner loading-lg"></span>
+          ) : (
+            <Component {...pageProps} />
+          )}
         </div>
         <Footer />
       </div>
