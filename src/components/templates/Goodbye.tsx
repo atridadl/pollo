bf2c563eba90275b2caae92ca5a3e32af668bf1e
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-07 22:38:47 -0600

    Better farewell...

--- src/components/templates/Goodbye.tsx
+++ src/components/templates/Goodbye.tsx
@@ -6,8 +6,8 @@ interface EmailTemplateProps {
 
 export const Goodbye: React.FC<Readonly<EmailTemplateProps>> = ({ name }) => (
   <div>
-    <h1>Welcome, {name}!</h1>
-    <p>{"We're sorry to see you go!"}</p>
+    <h1>Farewell, {name}...</h1>
+    <p>{"We're sorry to see you go."}</p>
     <p>
       Your data has been deleted, including all room history, user data, votes,
       etc.
