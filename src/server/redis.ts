cc7d6b01ae78b212502946554ddbfac39c96fdcc
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-29 16:52:23 -0600

    1.2.2

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,9 +1,6 @@
 import { Redis } from "@upstash/redis";
-import { env } from "~/env.mjs";
 import https from "https";
 
-export const redis = new Redis({
-  url: env.UPSTASH_REDIS_REST_URL,
-  token: env.UPSTASH_REDIS_REST_TOKEN,
+export const redis = Redis.fromEnv({
   agent: new https.Agent({ keepAlive: true }),
 });
