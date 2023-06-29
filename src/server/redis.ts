cfe7e387f0edae8c54558aaaf923f1ce228b066b
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-29 00:16:47 -0600

    Redis fixes again?

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,7 +1,9 @@
 import { Redis } from "@upstash/redis";
 import { env } from "~/env.mjs";
+import https from "https";
 
 export const redis = new Redis({
   url: env.UPSTASH_REDIS_REST_URL,
   token: env.UPSTASH_REDIS_REST_TOKEN,
+  agent: new https.Agent({ keepAlive: true }),
 });
