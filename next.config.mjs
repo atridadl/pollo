3e7cea20635194201db11105f281209a3b56d0e0
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-07-10 00:30:57 -0600

    copy button feedback and hmr

--- next.config.mjs
+++ next.config.mjs
@@ -13,12 +13,14 @@ const config = {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
   },
   webpack: (config, { dev, isServer }) => {
-    Object.assign(config.resolve.alias, {
-      "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-      react: "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-    });
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
     return config;
   },
 };
