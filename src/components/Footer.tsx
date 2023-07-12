0d2a392b52d8721cd38e5795b857e068a96ea506
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-07-11 18:19:45 -0600

    Small UI adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer: React.FC = () => {
     <footer className="footer footer-center h-12 p-2 bg-base-100 text-base-content">
       <div>
         <p>
-          Made with{" "}
-          <GiTechnoHeart className="inline-block text-primary text-lg animate-pulse" />{" "}
-          by{" "}
+          Made with{ " " }
+          <GiTechnoHeart className="inline-block text-primary text-lg animate-pulse" />{ " " }
+          by{ " " }
           <a
             className="link link-primary link-hover"
             href="https://atri.dad"
@@ -16,15 +16,15 @@ const Footer: React.FC = () => {
             target="_blank"
           >
             Atridad Lahiji
-          </a>{" "}
-          -{" "}
+          </a>{ " " }
+          -{ " " }
           <a
             className="link link-primary link-hover"
-            href={`https://github.com/atridadl/sprintpadawan/releases/tag/${packagejson.version}`}
+            href={ `https://github.com/atridadl/sprintpadawan/releases/tag/${packagejson.version}` }
             rel="noreferrer"
             target="_blank"
           >
-            v{packagejson.version}
+            v{ packagejson.version }
           </a>
         </p>
       </div>
