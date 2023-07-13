e79a1f32fc6da7cc4b68729a11c8d54a08889e53
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-07-12 18:15:19 -0600

    ably updates...

--- src/server/ably.ts
+++ src/server/ably.ts
@@ -2,13 +2,21 @@ import Ably from "ably";
 import { env } from "~/env.mjs";
 import type { EventType } from "../utils/types";
 
-const ably = new Ably.Realtime.Promise(env.ABLY_PRIVATE_KEY);
+const globalForAbly = globalThis as unknown as {
+  ably: Ably.Types.RealtimePromise;
+};
+
+export const ably =
+  globalForAbly.ably || new Ably.Realtime.Promise(env.ABLY_PRIVATE_KEY);
+
+if (env.NODE_ENV !== "production") globalForAbly.ably = ably;
 
 export const publishToChannel = async (
+  ablyInstance: Ably.Types.RealtimePromise,
   channel: string,
   event: EventType,
   message: string
 ) => {
-  const channelName = ably.channels.get(`${env.APP_ENV}-${channel}`);
+  const channelName = ablyInstance.channels.get(`${env.APP_ENV}-${channel}`);
   await channelName.publish(event, message);
 };
