eddbcdf20ad2c0dfd66e01114d1d1d854371aa48
Atridad Lahiji <me@atri.dad>
2023-07-17 22:27:03 -0600

    Wowee types are fucked for resend

--- src/components/templates/Welcome.tsx
+++ src/components/templates/Welcome.tsx
@@ -21,7 +21,7 @@ const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const Welcome: React.FC<Readonly<WelcomeTemplateProps>> = ({ name }) => (
+export const Welcome = ({ name }: WelcomeTemplateProps) => (
   <Html>
     <Head />
     <Preview>🎉 Welcome to Sprint Padawan! 🎉</Preview>
@@ -30,18 +30,18 @@ export const Welcome: React.FC<Readonly<WelcomeTemplateProps>> = ({ name }) => (
         <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
           <Section className="mt-[32px]">
             <Img
-              src={ `${baseUrl}/logo.webp` }
+              src={`${baseUrl}/logo.webp`}
               width="40"
               height="37"
-              alt={ `Sprint Padawan Logo` }
+              alt={`Sprint Padawan Logo`}
               className="my-0 mx-auto"
             />
           </Section>
           <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-            🎉 Welcome to Sprint Padawan, <strong>{ name }</strong>! 🎉
+            🎉 Welcome to Sprint Padawan, <strong>{name}</strong>! 🎉
           </Heading>
           <Text className="text-black text-[14px] leading-[24px]">
-            Hello { name },
+            Hello {name},
           </Text>
           <Text>Thank you for signing up for Sprint Padawan!</Text>
           <Text>
