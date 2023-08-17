a2ba66a77b365081b984cdfba6f24ec702bb0da0
Atridad Lahiji <me@atri.dad>
2023-08-17 16:40:09 -0600

    Weird middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { validateRequest } from "./server/unkey";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/(.*)"],
+  beforeAuth: async (req) => {
+    if (req.nextUrl.pathname.startsWith("/api/external")) {
+      const isValid = await validateRequest(req);
+      console.log("Is Valid?: ", isValid);
+      if (isValid) {
+        return NextResponse.next();
+      }
+      return new NextResponse("UNAUTHORIZED", { status: 403 });
+    }
+  },
 });
 
 export const config = {
--- src/pages/api/external/ping.ts
+++ src/pages/api/external/ping.ts
@@ -1,13 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { validateRequest } from "~/server/unkey";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const isValid = await validateRequest(req, res);
-
-  if (isValid) {
-    res.status(200).json({ result: "Pong!" });
-  }
+  console.log("Made it to the function!");
+  res.status(200).json({ result: "Pong!" });
 }
