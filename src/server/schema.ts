2e4fd91c6ea7eb5c856849de9107fe208fd04e5d
Atridad Lahiji <me@atri.dad>
2023-08-18 23:49:35 -0600

    Index + deps

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -4,6 +4,7 @@ import {
   varchar,
   boolean,
   json,
+  index,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
@@ -22,15 +23,23 @@ export const roomsRelations = relations(rooms, ({ many }) => ({
   logs: many(logs),
 }));
 
-export const votes = pgTable("Vote", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
-  created_at: timestamp("created_at").defaultNow(),
-  userId: varchar("userId", { length: 255 }).notNull(),
-  roomId: varchar("roomId", { length: 255 })
-    .notNull()
-    .references(() => rooms.id, { onDelete: "cascade" }),
-  value: varchar("value", { length: 255 }).notNull(),
-});
+export const votes = pgTable(
+  "Vote",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    created_at: timestamp("created_at").defaultNow(),
+    userId: varchar("userId", { length: 255 }).notNull(),
+    roomId: varchar("roomId", { length: 255 })
+      .notNull()
+      .references(() => rooms.id, { onDelete: "cascade" }),
+    value: varchar("value", { length: 255 }).notNull(),
+  },
+  (table) => {
+    return {
+      userIdx: index("user_idx").on(table.userId),
+    };
+  }
+);
 
 export const votesRelations = relations(votes, ({ one }) => ({
   room: one(rooms, {
@@ -39,20 +48,28 @@ export const votesRelations = relations(votes, ({ one }) => ({
   }),
 }));
 
-export const logs = pgTable("Log", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
-  created_at: timestamp("created_at").defaultNow(),
-  userId: varchar("userId", { length: 255 })
-    .notNull()
-    .references(() => rooms.id, { onDelete: "cascade" }),
-  roomId: varchar("roomId", { length: 255 })
-    .notNull()
-    .references(() => rooms.id, { onDelete: "cascade" }),
-  scale: varchar("scale", { length: 255 }),
-  votes: json("votes"),
-  roomName: varchar("roomName", { length: 255 }),
-  storyName: varchar("storyName", { length: 255 }),
-});
+export const logs = pgTable(
+  "Log",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    created_at: timestamp("created_at").defaultNow(),
+    userId: varchar("userId", { length: 255 })
+      .notNull()
+      .references(() => rooms.id, { onDelete: "cascade" }),
+    roomId: varchar("roomId", { length: 255 })
+      .notNull()
+      .references(() => rooms.id, { onDelete: "cascade" }),
+    scale: varchar("scale", { length: 255 }),
+    votes: json("votes"),
+    roomName: varchar("roomName", { length: 255 }),
+    storyName: varchar("storyName", { length: 255 }),
+  },
+  (table) => {
+    return {
+      userIdx: index("user_idx").on(table.userId),
+    };
+  }
+);
 
 export const logsRelations = relations(logs, ({ one }) => ({
   room: one(rooms, {
