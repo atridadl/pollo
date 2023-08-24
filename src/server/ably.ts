7e28aa3833908133184fde89c1cf0e002a2a83f8
Atridad Lahiji <me@atri.dad>
2023-08-24 17:49:00 -0600

    2.1.0 Living on the EDGE BAYBE!

--- src/server/ably.ts
+++ src/server/ably.ts
@@ -1,4 +1,4 @@
-import Ably from "ably";
+// import Ably from "ably";
 import { env } from "~/env.mjs";
 import type { EventType } from "../utils/types";
 
@@ -7,7 +7,18 @@ export const publishToChannel = async (
   event: EventType,
   message: string
 ) => {
-  const ably = new Ably.Rest.Promise(env.ABLY_PRIVATE_KEY);
-  const ablyChannel = ably.channels.get(`${env.APP_ENV}-${channel}`);
-  await ablyChannel.publish(event, message, { quickAck: true });
+  await fetch(
+    `https://rest.ably.io/channels/${env.APP_ENV}-${channel}/messages`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${btoa(env.ABLY_PRIVATE_KEY)}`,
+      },
+      body: JSON.stringify({
+        name: event,
+        data: message,
+      }),
+    }
+  );
 };
