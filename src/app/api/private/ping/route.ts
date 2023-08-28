b23761c1067506dd77d56dafc3d56c9085a0451b
Atridad Lahiji <me@atri.dad>
2023-08-28 00:31:47 -0600

    Remove email and move fully to app router

--- src/app/api/private/ping/route.ts
+++ src/app/api/private/ping/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
-export const config = {
-  runtime: "edge",
-  regions: ["pdx1"],
-};
+export const runtime = "edge";
+export const preferredRegion = ["pdx1"];
 
-export default function handler() {
+function handler() {
   return NextResponse.json(
     { message: "Private Pong!" },
     { status: 200, statusText: "SUCCESS" }
   );
 }
+
+export { handler as GET };
