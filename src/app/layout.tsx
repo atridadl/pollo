414814cff0893fd79692444e5b74d7a10c3dac77
Atridad Lahiji <me@atri.dad>
2023-08-29 18:42:10 -0600

    Fixed responsiveness issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className="h-[100%] w-[100%] fixed overflow-y-auto">
-        <body className="block h-[100%]">
+        <body className="h-[100%] w-[100%] fixed overflow-y-auto">
           <Header title="Sprint Padawan" />
           <div className="flex flex-row items-center justify-center min-h-[calc(100%-114px)]">
             <Provider>{children}</Provider>
