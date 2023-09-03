d6fcc50262a2d86406cc0a8bc1f2c26e12af30ae
Atridad Lahiji <me@atri.dad>
2023-09-03 16:04:35 -0600

    Edgy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,9 +4,8 @@ import { GiStarFormation } from "react-icons/gi";
 import { isAdmin, isVIP } from "@/utils/helpers";
 import { currentUser } from "@clerk/nextjs";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const fetchCache = "force-no-store";
+export const runtime = "edge";
+export const preferredRegion = ["pdx1"];
 
 export default async function Dashboard() {
   const user = await currentUser();
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,19 @@
+import { json2csv } from "csv42";
+
+export function jsonToCsv(jsonObject: Array<object>, fileName: string) {
+  const csv = json2csv(jsonObject);
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", fileName);
+  link.style.visibility = "hidden";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
 export function isAdmin(meta: UserPublicMetadata | undefined) {
   return (meta?.isAdmin as boolean | undefined) || false;
 }
