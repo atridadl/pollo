a9bf7365cc39934b3c2e33e1b3d4bceb1cefde38
Atridad Lahiji <me@atri.dad>
2023-09-09 19:41:37 -0600

    One more fix

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,11 +1,5 @@
 import VoteUI from "@/app/_components/VoteUI";
 
-// export const runtime = "edge";
-// export const preferredRegion = ["pdx1"];
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
-// export const fetchCache = "force-no-store";
-
 export default function Room() {
   return (
     <div className="flex flex-col items-center justify-center text-center gap-2">
