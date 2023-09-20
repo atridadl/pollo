8a0a02528e1365f9844a839e47b2fc7f48f1d053
Atridad Lahiji <me@atri.dad>
2023-09-19 23:00:00 -0600

    Sign-in changes

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -5,17 +5,7 @@ import { SignUp } from "@clerk/nextjs";
 export const dynamic = "force-static";
 
 const SignUpPage = () => (
-  <div style={styles}>
-    <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-  </div>
+  <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
 );
 
 export default SignUpPage;
-
-const styles = {
-  width: "100vw",
-  height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
