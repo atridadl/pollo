393fdfe40cf9057c863f20818c4c261743072c0b
atridadl <me@atri.dad>
2023-11-23 00:36:16 -0700

    Style updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,36 +2,18 @@ export default function Index() {
   return (
     <div className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4">
       <h1 className="text-3xl sm:text-6xl font-bold">
-        <span className="inline-grid">
-          <span
-            className="pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text blur-2xl [-webkit-text-fill-color:transparent] [transform:translate3d(0,0,0)] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
-            aria-hidden="true"
-          >
-            Sprint Padawan
-          </span>
-          <span className="[&::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&::selection]:bg-blue-700/20 [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
-            Sprint Padawan
-          </span>
+        <span className="bg-gradient-to-r from-primary via-accent to-secondary bg-clip-text text-transparent box-decoration-clone">
+          Sprint Padawan
         </span>
       </h1>
 
       <h2 className="my-4 text-xl sm:text-3xl font-bold">
-        A{" "}
-        <span className="bg-gradient-to-br from-pink-600 to-pink-400 bg-clip-text text-transparent box-decoration-clone">
-          scrum poker{" "}
-        </span>{" "}
-        tool that helps{" "}
-        <span className="bg-gradient-to-br from-purple-600 to-purple-400 bg-clip-text text-transparent box-decoration-clone">
-          agile teams{" "}
-        </span>{" "}
-        plan their sprints in{" "}
-        <span className="bg-gradient-to-br from-cyan-600 to-cyan-400 bg-clip-text text-transparent box-decoration-clone">
-          real-time
-        </span>
-        .
+        A <span className="text-primary">scrum poker </span> tool that helps{" "}
+        <span className="text-secondary">agile teams </span> plan their sprints
+        in <span className="text-accent">real-time</span>.
       </h2>
 
-      <div className="card card-compact bg-secondary text-black font-bold text-left">
+      <div className="card glass card-compact bg-secondary text-black font-bold text-left">
         <div className="card-body">
           <h2 className="card-title">Features:</h2>
           <ul>
