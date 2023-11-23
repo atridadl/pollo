d99e8953c3deca2af2bd4850c919e6f6e9f8e8b9
atridadl <me@atri.dad>
2023-11-22 22:23:00 -0700

    Fixed CSS

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,9 +2,16 @@ export default function Index() {
   return (
     <div className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4">
       <h1 className="text-3xl sm:text-6xl font-bold">
-        Sprint{" "}
-        <span className="bg-gradient-to-br from-pink-600 to-cyan-400 bg-clip-text text-transparent box-decoration-clone">
-          Padawan
+        <span className="inline-grid">
+          <span
+            className="pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-background-clip: text] blur-2xl [-webkit-text-fill-color:transparent] [transform:translate3d(0,0,0)] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+            aria-hidden="true"
+          >
+            Sprint Padawan
+          </span>
+          <span className="[&::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-background-clip: text] [-webkit-text-fill-color:transparent] [&::selection]:bg-blue-700/20 [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
+            Sprint Padawan
+          </span>
         </span>
       </h1>
 
