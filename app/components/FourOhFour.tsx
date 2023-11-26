97d915145214e57d707bfcba7cb204db63c79cd6
atridadl <me@atri.dad>
2023-11-25 23:48:10 -0700

    Split out error bound component for rooms and improved the message

---
+++ app/components/FourOhFour.tsx
@@ -0,0 +1,28 @@
+import { Link } from "@remix-run/react";
+
+export default function FourOhFour() {
+  return (
+    <span className="text-center">
+      <h1 className="text-5xl font-bold m-2">4️⃣0️⃣4️⃣</h1>
+      <h1 className="text-5xl font-bold m-2">
+        Oops! This room does not appear to exist, or may have been deleted! 😢
+      </h1>
+      <h2 className="text-2xl font-bold m-2">
+        If you believe you reached this page in error, please file an issue{" "}
+        <a
+          href="https://github.com/atridadl/sprintpadawan/issues/new"
+          className="link link-secondary"
+        >
+          here
+        </a>
+      </h2>
+      <Link
+        about="Back to home."
+        to="/"
+        className="btn btn-secondary normal-case text-xl m-2"
+      >
+        Back to Home
+      </Link>
+    </span>
+  );
+}
