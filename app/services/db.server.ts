f409460b47c5a46d8c598d9accff420097ed2e18
atridadl <me@atri.dad>
2024-01-13 22:22:03 -0700

    Revised keepalive setup (turns out its flyproxy)

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,7 +1,10 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema.server";
 import "dotenv/config";
 
-const queryClient = postgres(process.env.DATABASE_URL!, {keep_alive: 10000});
+const queryClient = postgres(process.env.DATABASE_URL!, {
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
+});
 export const db = drizzle(queryClient, { schema });
