a592ed320cdf5a736c042b34895d44f49b8a49b9
Atridad Lahiji <me@atri.dad>
2024-04-09 11:21:55 -0600

    ROOT URL USAGE

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,5 +1,26 @@
 import { SignIn } from "@clerk/remix";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  return json({
+    ENV: {
+      ROOT_URL: process.env.ROOT_URL,
+    },
+  });
+}
 
 export default function SignInPage() {
-  return <SignIn />;
+  const { ENV } = useLoaderData<typeof loader>();
+
+  return (
+    <main className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4 min-h-[100%]">
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl="/sign-up"
+        redirectUrl={ENV.ROOT_URL ? ENV.ROOT_URL : "/"}
+      />{" "}
+    </main>
+  );
 }
--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,5 +1,26 @@
 import { SignUp } from "@clerk/remix";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader() {
+  return json({
+    ENV: {
+      ROOT_URL: process.env.ROOT_URL,
+    },
+  });
+}
 
 export default function SignUpPage() {
-  return <SignUp />;
+  const { ENV } = useLoaderData<typeof loader>();
+
+  return (
+    <main className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4 min-h-[100%]">
+      <SignUp
+        path="/sign-in"
+        routing="path"
+        signInUrl="/sign-up"
+        redirectUrl={ENV.ROOT_URL ? ENV.ROOT_URL : "/"}
+      />{" "}
+    </main>
+  );
 }
