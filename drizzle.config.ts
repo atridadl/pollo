4ff86b873a5b27e3009e425e7a6590ae295fb8cd
Atridad Lahiji <me@atri.dad>
2024-06-03 17:04:28 -0600

    Updated Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
-export default {
+export default defineConfig({
+  dialect: "postgresql", // "postgresql" | "mysql"
   schema: "./app/services/schema.server.ts",
   out: "./drizzle/generated",
-  driver: "pg",
-  breakpoints: true,
   dbCredentials: {
-    connectionString: `${process.env.DATABASE_URL}`,
-  },
-} satisfies Config;
+    url: `${process.env.DATABASE_URL}`
+  }
+})
